d688409aea9dcb272a9e1c48ff55a2bfb79825dd
taylordotson <33382665+taylordotson@users.noreply.github.com>
2018-02-01 13:20:01 -0600

    first commit

---
+++ grades.js
@@ -0,0 +1,10 @@
+const scores = [82, 71, 62, 95, 55, 98, 69, 72, 78, 84, 64, 58, 87, 60]
+const grades = {}
+
+for (let i = 0; i < scores.length; i++) {
+    if  scores >= 90;
+    if  90 > scores >= 80;
+    if  80 > scores >= 70;
+    if  70 > scores >= 60;
+    if  60 > scores;
+}
